73448f4f4aca2d69fde76ade38794fa5b6a398b1
ALLOJU <mounika.alloju@gmail.com>
2022-05-12 10:42:10 +0530

    UC1 with online book store with front end added

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+
+import RouterComponent from './router/RouterComponent';
+import CbFooter from "./component/utils/CbFooter";
+
+function App() {
+    return (
+        <BrowserRouter>
+            <RouterComponent/>
+
+        </BrowserRouter>
+    );
+}
+
+export default App;
